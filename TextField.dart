22694919108d17f210e561a180cf39c379e7e305
Mir Fahim <54933139+mirfahim@users.noreply.github.com>
2021-08-03 16:39:20 +0600

    Create TextField.dart

---
+++ TextField.dart
@@ -0,0 +1,28 @@
+TextField(
+                        autofocus: false,
+                        focusNode: FocusNode(),
+                        controller: addresss,
+
+                        decoration: InputDecoration(
+                        //  prefixIcon: Icon(Icons.search, color: Colors.black,),
+                            hintText: 'Your Address',
+                            border: OutlineInputBorder()),
+                        onTap: () async {
+                          print("Search Result");
+final sessionToken = Uuid().v4();
+final Suggestion result = await showSearch(
+  context: context,
+  delegate: AddressSearch(sessionToken));
+if(result != null){
+  setState(() {
+   addresss.text = result.description;
+
+  });
+} else {
+  setState(() {
+    addresss.text = SharedPref.location;
+  });
+}
+
+                        },
+                      ),
